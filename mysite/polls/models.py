711dee8907dd2d47605cdf12d4e1d94d95833417
NarawishS <narawish.s@ku.th>
2020-09-19 17:16:48 +0700

    change some function, set some code for admin

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,30 +1,32 @@
-import datetime
-
 from django.db import models
 from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    end_date = models.DateTimeField('closing date')
+    pub_date = models.DateTimeField(verbose_name='date published')
+    end_date = models.DateTimeField(verbose_name='closed date')
 
     def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
         now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+        return now - timezone.timedelta(days=1) <= self.pub_date <= now
 
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
 
     def is_published(self):
-        return timezone.now() >= self.pub_date
+        return self.pub_date <= timezone.now()
 
     def can_vote(self):
-        return self.end_date < timezone.now()
+        return self.pub_date <= timezone.now() <= self.end_date
+
+    can_vote.admin_order_field = 'end_date'
+    can_vote.boolean = True
+    can_vote.short_description = 'Open?'
 
 
 class Choice(models.Model):
