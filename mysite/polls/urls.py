2ee43038ee0977b5c929e86175b0c601af92acef
NarawishS <narawish.s@ku.th>
2020-10-08 22:55:06 +0700

    remove redundant code

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('reset', views.reset_index, name='reset_polls')
 ]
