6f3a252ca39060453dd823cfff5f33c561bf814e
Narawish Sangsiriwut <narawish.s@ku.th>
2020-11-01 00:17:48 +0700

    change dir

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
 
-from .models import Choice, Question
+from mysite.polls.models import Choice, Question
 
 
 class IndexView(generic.ListView):
