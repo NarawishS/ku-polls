9f1b9ebad848610f87af2a6e4849e92eeba33d64
Narawish Sangsiriwut <narawish.s@ku.th>
2020-11-01 17:36:40 +0700

    refactor dir
    because of mysite.mysite in old dir there are bug in module when separate file in dir

--- polls/models.py
+++ polls/models.py
@@ -66,7 +66,6 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.choice_text
