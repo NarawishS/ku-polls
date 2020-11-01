9321c3886ed4cdf45b76ce298d665b31de4510f8
Narawish Sangsiriwut <narawish.s@ku.th>
2020-11-02 00:00:08 +0700

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -20,7 +20,9 @@ logging.basicConfig(level=logging.INFO)
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        ip = x_forwarded_for.split(',')[0]
+        ip = x_forwarded_for.split(',')[-1].strip()
+    elif request.META.get('HTTP_X_REAL_IP'):
+        ip = request.META.get('HTTP_X_REAL_IP')
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
