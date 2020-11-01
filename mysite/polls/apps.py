ecd3851c7cd8a6da9b74eb8cc801b33f21ba2422
Narawish Sangsiriwut <narawish.s@ku.th>
2020-11-01 17:14:19 +0700

    f

--- mysite/polls/apps.py
+++
@@ -1,8 +0,0 @@
-"""Configure for an application."""
-from django.apps import AppConfig
-
-
-class PollsConfig(AppConfig):
-    """Config for apps when use in INSTALLED_APPS."""
-
-    name = 'polls'
